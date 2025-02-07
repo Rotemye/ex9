35f3eabb9141535f16c38d0f8331cf033a5ae664
rotemye <rotem.yemini@gmail.com>
2025-02-07 14:20:05 +0200

    adding a file with the answers to question 1

--- q1.sh
+++ q1.sh
@@ -1,1 +1,18 @@
-This is q1 answer
+#!/bin/bash
+#a
+awk 'END {print NR}' aliceinwonderland.txt
+
+
+#b
+awk '{for (i=2; i<NF; i++) if ($i == "Alice") count++} END {print count}' aliceinwonderland.txt
+
+
+#c
+awk '{ for (i=1; i<=NF; i++) { if ($i ~ /^[a-zA-Z0-9]+$/) word[$i]++ }} END { for (w in word) if (word[w] == 1) print w }' aliceinwonderland.txt
+
+#d
+grep -o '\w\+' aliceinwonderland.txt | sort | uniq -c | sort -h | tail -5
+
+#e
+awk '{ for (i=1; i<=NF; i++) { total_length += length($i); word_count++;}} END { if (word_count > 0) print total_length / word_count}' aliceinwonderland.txt 
+
